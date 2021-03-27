05b155f20c9c880c3aabd6617b82730abc94fc88
Marc Wimmer <marc@itewimmer.de>
2021-03-27 08:07:25 +0100

    hotfix: does not work

---
+++ odoobrowser/README.md
@@ -0,0 +1,5 @@
+# Odoo-Browser
+
+## Features
+
+  * Quickly shows xmlids, methods in a webview with link to files
---
+++ odoobrowser/src/extension.ts
@@ -0,0 +1,75 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import {PythonShell} from 'python-shell';
+
+
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "odoobrowser" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	const cmd_hello = vscode.commands.registerCommand('odoobrowser.helloWorld', () => {
+		// The code you place here will be executed every time your command is executed
+
+		// Display a message box to the user
+		const panel = vscode.window.createWebviewPanel(
+			'catCoding',
+			'Cat Coding',
+			vscode.ViewColumn.One,
+			{}
+		);
+	
+		panel.title = "Odoo Browser"
+		panel.webview.html = getWebviewContent("http://10.10.173.111:8620/xmlids");
+	
+		  // And schedule updates to the content every second
+		vscode.window.showInformationMessage('Hello World from OdooBrowser!');
+	});
+
+	const cmd_bye = vscode.commands.registerCommand('odoobrowser.byeWorld', () => {
+		vscode.window.showInformationMessage('Good Bye from OdooBrowser!');
+	});
+
+	const cmd2 = vscode.commands.registerCommand('odoobrowser.command2', () => {
+		vscode.window.showInformationMessage('cmd2');
+	});
+	context.subscriptions.push(cmd_hello, cmd_bye, cmd2);
+
+}
+
+function getWebviewContent(url: string) {
+	return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+	  <meta charset="UTF-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	  <title>Cat Coding</title>
+  </head>
+  <script
+  src="https://code.jquery.com/jquery-3.6.0.min.js"
+  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+  crossorigin="anonymous"></script>
+  <script>
+  $(document).ready(function() {
+	  $("span.test").test("HI");
+
+  });
+  })
+  </script>
+  <body>
+	${url}
+	<span class='test'></span>
+  </body>
+  </html>`;
+  }
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
